05e1f35c5d87ef2f22e09fe516984e50eeb923dd
MaximeOriot <maxime9278@gmail.com>
2025-06-25 16:24:31 +0200

    add syne font

--- src/features/Home/HomePage.tsx
+++ src/features/Home/HomePage.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo-squizzit-removed-bg.png';
 
 function HomePage() {
   return (
-    <div className="flex items-center justify-between h-screen px-12 bg-gradient-to-b from-sky-300 to-sky-800">
+    <div className="container flex items-center justify-between h-screen px-12 bg-gradient-to-b from-sky-300 to-sky-800">
       <div className="text-3xl font-bold max-w-xl">
         Découvrez le plaisir d'apprendre avec <span className="text-blue-900">S-quizz-it</span> !!!
       </div>
