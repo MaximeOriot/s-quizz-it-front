6c44a3b867ea58720e866b3a8c18a89661ca60e1
MaximeOriot <maxime9278@gmail.com>
2025-06-25 15:22:10 +0200

    add tailwindcss

--- src/features/Home/HomePage.tsx
+++ src/features/Home/HomePage.tsx
@@ -1,14 +1,17 @@
 import './HomePage.css'
+import logo from '../../assets/logo-squizzit-removed-bg.png';
 
 function HomePage() {
-
   return (
-    <>
-      <div>
-        Découvrez le plaisir d'apprendre avec S-quizz-it !!!
+    <div className="flex items-center justify-between h-screen px-12 bg-gray-50">
+      <div className="text-3xl font-bold max-w-xl">
+        Découvrez le plaisir d'apprendre avec <span className="text-blue-600">S-quizz-it</span> !!!
       </div>
-    </>
-  )
+      <div>
+        <img src={logo} alt="Logo S-quizz-it" className="w-[300px]" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage
