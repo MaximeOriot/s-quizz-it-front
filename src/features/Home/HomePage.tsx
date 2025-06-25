5489d0286f58ca7c73cc9d81a92364698329c1b7
MaximeOriot <maxime9278@gmail.com>
2025-06-25 14:44:38 +0200

    rename files and create folders

---
+++ src/features/Home/HomePage.tsx
@@ -0,0 +1,14 @@
+import './HomePage.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Découvrez le plaisir d'apprendre avec S-quizz-it !!!
+      </div>
+    </>
+  )
+}
+
+export default App
