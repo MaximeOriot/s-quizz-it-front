115d4a6a8abeb4c881dad656550ec1edfc98e05d
MaximeOriot <maxime9278@gmail.com>
2025-06-25 16:11:22 +0200

    bg color

--- src/features/Home/HomePage.tsx
+++ src/features/Home/HomePage.tsx
@@ -3,9 +3,9 @@ import logo from '../../assets/logo-squizzit-removed-bg.png';
 
 function HomePage() {
   return (
-    <div className="flex items-center justify-between h-screen px-12 bg-gray-50">
+    <div className="flex items-center justify-between h-screen px-12 bg-gradient-to-b from-sky-300 to-sky-800">
       <div className="text-3xl font-bold max-w-xl">
-        Découvrez le plaisir d'apprendre avec <span className="text-blue-600">S-quizz-it</span> !!!
+        Découvrez le plaisir d'apprendre avec <span className="text-blue-900">S-quizz-it</span> !!!
       </div>
       <div>
         <img src={logo} alt="Logo S-quizz-it" className="w-[300px] h-auto" />
