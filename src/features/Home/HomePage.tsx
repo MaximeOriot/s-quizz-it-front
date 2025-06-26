388e57df18737b109c6ee8e31484a9707f1b156b
GabsBaquie <GabsBaquie@users.noreply.github.com>
2025-06-26 12:01:30 +0200

    feat: configure Tailwind CSS and update font usage in HomePage

--- src/features/Home/HomePage.tsx
+++ src/features/Home/HomePage.tsx
@@ -1,14 +1,13 @@
-import './HomePage.css'
 import logo from '../../assets/logo-squizzit-removed-bg.png';
 
 function HomePage() {
   return (
-    <div className="container flex items-center justify-between h-screen px-12 bg-gradient-to-b from-sky-300 to-sky-800">
-      <div className="text-3xl font-bold max-w-xl">
+    <div className="font-syne-bold flex items-center justify-between h-screen px-12 bg-gradient-to-b from-sky-300 to-sky-800">
+      <div className="text-3xl font-syne-bold max-w-xl">
         Découvrez le plaisir d'apprendre avec <span className="text-blue-900">S-quizz-it</span> !!!
       </div>
       <div>
-        <img src={logo} alt="Logo S-quizz-it" className="w-[300px] h-auto" />
+        <img src={logo} alt="Logo S-quizz-it" className="w-[500px] h-auto" />
       </div>
     </div>
   );
