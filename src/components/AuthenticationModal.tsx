f5af9bc701af3c14d6148d48ecd53fac7b47402c
MaximeOriot <maxime9278@gmail.com>
2025-06-26 16:43:13 +0200

    update auth modal style

--- src/components/AuthenticationModal.tsx
+++ src/components/AuthenticationModal.tsx
@@ -28,20 +28,20 @@ export default function AuthModal({ isOpen, isRegister, onClose, onLoginSuccess 
   };
 
   return (
-    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-6 w-full max-w-md shadow-lg relative">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50 bg-black/50 backdrop-blur-sm">
+      <div className="relative w-full max-w-sm p-6 shadow-xl bg-gradient-to-b from-blue-400 to-blue-600 rounded-xl">
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 text-gray-600 hover:text-black text-xl"
+          className="absolute text-xl text-gray-600 top-2 right-2 hover:text-black"
         >
           ×
         </button>
 
-        <div className="flex mb-4 justify-center">
+        <div className="flex justify-center mb-4">
           <button
             onClick={() => setOnRegister(false)}
             className={`px-4 py-2 font-semibold ${
-              !onRegister ? "border-b-2 border-blue-900 text-blue-900" : "text-gray-500"
+              !onRegister ? "border-b-2 border-secondary text-secondary" : "text-gray-500"
             }`}
           >
             Connexion
@@ -49,7 +49,7 @@ export default function AuthModal({ isOpen, isRegister, onClose, onLoginSuccess 
           <button
             onClick={() => setOnRegister(true)}
             className={`px-4 py-2 font-semibold ${
-              onRegister ? "border-b-2 border-blue-900 text-blue-900" : "text-gray-500"
+              onRegister ? "border-b-2 border-secondary text-secondary" : "text-gray-500"
             }`}
           >
             Inscription
@@ -58,19 +58,19 @@ export default function AuthModal({ isOpen, isRegister, onClose, onLoginSuccess 
 
         {!onRegister ? (
           <form className="flex flex-col gap-4" onSubmit={handleLoginSubmit}>
-            <input type="email" placeholder="Email" className="border px-3 py-2 rounded" />
-            <input type="password" placeholder="Mot de passe" className="border px-3 py-2 rounded" />
-            <button type="submit" className="bg-blue-900 text-white py-2 rounded hover:bg-purple-600">
+            <input type="email" placeholder="Email" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <input type="password" placeholder="Mot de passe" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <button type="submit" className="py-2 mt-2 font-bold transition rounded-full shadow-md text-secondary bg-gradient-to-r from-yellow-400 to-yellow-500 hover:from-yellow-300 hover:to-yellow-400">
               Se connecter
             </button>
           </form>
         ) : (
           <form className="flex flex-col gap-4" onSubmit={handleLoginSubmit}>
-            <input type="text" name="username" placeholder="Nom" className="border px-3 py-2 rounded" />
-            <input type="email" name="email" placeholder="Email" className="border px-3 py-2 rounded" />
-            <input type="password" name="password" placeholder="Mot de passe" className="border px-3 py-2 rounded" />
-            <input type="password" name="confirmPassword" placeholder="Confirmer le mot de passe" className="border px-3 py-2 rounded" />
-            <button type="submit" className="bg-blue-900 text-white py-2 rounded hover:bg-purple-600">
+            <input type="text" name="username" placeholder="Nom" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <input type="email" name="email" placeholder="Email" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <input type="password" name="password" placeholder="Mot de passe" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <input type="password" name="confirmPassword" placeholder="Confirmer le mot de passe" className="py-2 text-center bg-yellow-100 border border-purple-700 rounded-md placeholder-secondary" />
+            <button type="submit" className="py-2 mt-2 font-bold transition rounded-full shadow-md text-secondary bg-gradient-to-r from-yellow-400 to-yellow-500 hover:from-yellow-300 hover:to-yellow-400">
               S'inscrire
             </button>
           </form>
