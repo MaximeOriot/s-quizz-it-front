134eec4d7bf5bbb6cc2090a8f8ea9b048f840635
MaximeOriot <maxime9278@gmail.com>
2025-07-01 11:29:38 +0200

    interfaces + api file + questions services

--- src/features/Waiting/WaitingRoom.tsx
+++ src/features/Waiting/WaitingRoom.tsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
 import Header from "../../components/ui/Header";
 import Button from "../../components/ui/Button";
+import type { WaitingPlayer } from "./models/waitingPlayer";
+import type { Quizz } from "../../models/quizz";
 
 interface WaitingRoomProps {
   isQuickPlay: boolean;
+  players: WaitingPlayer[];
+  quizz?: Quizz;
 }
 
-const players = [
-  { name: "Vous", ready: true, avatar: './src/assets/avatars/pomme.png' },
-  { name: "Player 2", ready: true, avatar: './src/assets/avatars/poire.png' },
-];
-
-const quiz = {
-  title: "Nom du quizz",
-  description: "Ceci est la description du quizz.",
-  themes: ["Theme 1", "Theme 2"]
-};
-
-function WaitingRoom({ isQuickPlay }: WaitingRoomProps) {
+function WaitingRoom({ isQuickPlay, players, quizz }: WaitingRoomProps) {
   const [isReady, setIsReady] = useState(false);
 
   const getReady = () => {
@@ -36,13 +29,13 @@ function WaitingRoom({ isQuickPlay }: WaitingRoomProps) {
                 <li key={index} className="flex items-center mb-2">
                   <img
                     src={player.avatar}
-                    alt={`Avatar de ${player.name}`}
+                    alt={`Avatar de ${player.pseudo}`}
                     className="object-cover w-8 h-8 mr-3 border-2 rounded-full border-thirdary"
                   />
-                  <span className="text-secondary">{player.name}</span>
+                  <span className="text-secondary">{player.pseudo}</span>
                   <input
                     type="checkbox"
-                    checked={player.ready}
+                    checked={player.isReady}
                     readOnly
                     className="w-4 h-4 ml-2 accent-thirdary"
                   />
@@ -67,22 +60,24 @@ function WaitingRoom({ isQuickPlay }: WaitingRoomProps) {
           )}
         </div>
 
-        <div className="flex items-center justify-center w-1/2">
+        {quizz  && (
+          <div className="flex items-center justify-center w-1/2">
           <div className="w-full max-w-xl px-10 py-8 text-center bg-secondary rounded-2xl">
-            <h2 className="mb-2 text-2xl font-bold text-primary">{quiz.title}</h2>
-            <p className="mb-6 text-primary">{quiz.description}</p>
+            <h2 className="mb-2 text-2xl font-bold text-primary">{quizz.label}</h2>
+            <p className="mb-6 text-primary">{quizz.description}</p>
             <div className="flex justify-center gap-4">
-              {quiz.themes.map((theme, idx) => (
+              {quizz.themes.map((theme, idx) => (
                 <span
                   key={idx}
                   className="px-4 py-1 text-sm font-semibold rounded-full bg-sky-400"
                 >
-                  {theme}
+                  {theme.label}
                 </span>
               ))}
             </div>
           </div>
         </div>
+        )}
       </div>
     </div>
   );
